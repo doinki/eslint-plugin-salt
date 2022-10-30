380d1996e061887d492dc46a708a1c0fe5c45220
doinki <doinki94@gmail.com>
2022-10-30 10:23:24 +0900

    feat: Add tests

---
+++ tests/hooks.test.ts
@@ -0,0 +1,262 @@
+import { RuleTester } from 'eslint';
+
+import Hooks from '../src/lib/rules/hooks';
+
+(RuleTester as any).setDefaultConfig({
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+});
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('hooks', Hooks, {
+  valid: [
+    `
+import { useCallback, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const handleClick = useCallback(() => {
+    setA(a + 1);
+    setB(b + 1);
+  }, [a, b]);
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+    `
+import { useEffect, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useEffect(() => {
+    console.log(a, b);
+  }, [a, b]);
+
+  return <div>App</div>;
+}
+
+export default Test;
+`,
+    `
+import { useLayoutEffect, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useLayoutEffect(() => {
+    console.log(a, b);
+  }, [a, b]);
+
+  return <div>App</div>;
+}
+
+export default Test;
+`,
+    `
+import { useMemo, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const c = useMemo(() => a + b, [a, b]);
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+    `
+import { useState } from 'react';
+
+import useCustomHook from './useCustomHook';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useCustomHook(() => {
+    console.log(a, b);
+  }, [b, a])
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+  ],
+  invalid: [
+    {
+      code: `
+import { useCallback, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const handleClick = useCallback(() => {
+    setA(a + 1);
+    setB(b + 1);
+  }, [b, a]);
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+      errors: [
+        {
+          message: "Hook's Dependency List should be sorted alphabetically",
+          type: 'ArrayExpression',
+        },
+      ],
+      output: `
+import { useCallback, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const handleClick = useCallback(() => {
+    setA(a + 1);
+    setB(b + 1);
+  }, [a, b]);
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+    },
+    {
+      code: `
+import { useEffect, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useEffect(() => {
+    console.log(a, b);
+  }, [b, a])
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+      errors: [
+        {
+          message: "Hook's Dependency List should be sorted alphabetically",
+          type: 'ArrayExpression',
+        },
+      ],
+      output: `
+import { useEffect, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useEffect(() => {
+    console.log(a, b);
+  }, [a, b])
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+    },
+    {
+      code: `
+import { useLayoutEffect, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useLayoutEffect(() => {
+    console.log(a, b);
+  }, [b, a])
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+      errors: [
+        {
+          message: "Hook's Dependency List should be sorted alphabetically",
+          type: 'ArrayExpression',
+        },
+      ],
+      output: `
+import { useLayoutEffect, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useLayoutEffect(() => {
+    console.log(a, b);
+  }, [a, b])
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+    },
+    {
+      code: `
+import { useMemo, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const c = useMemo(() => a + b, [b, a]);
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+      errors: [
+        {
+          message: "Hook's Dependency List should be sorted alphabetically",
+          type: 'ArrayExpression',
+        },
+      ],
+      output: `
+import { useMemo, useState } from 'react';
+
+const Test = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const c = useMemo(() => a + b, [a, b]);
+
+  return <div>Test</div>;
+}
+
+export default Test;
+`,
+    },
+  ],
+});
